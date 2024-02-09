5bf8fdfb0effa9a74baaf485320912f4e5714309
SAP 개발자 <quelqunadire@gmail.com>
2024-02-09 17:24:08 +0900

    대소문자 바꿔서 출력

--- General Practice/Programmers_basic_char.java
+++ General Practice/Programmers_basic_char.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+// isUpper(Lower)Case() -> boolean
+// toUpper(Lower)Case() -> return 
+
+// 주의 :둘다 Character Class의 메서드이므로 char 변수 선언해서 String.charAt(int) 로 자른 뒤 사용해야함
+
 public class Programmers_basic_char {
 
     public static void main(String[] args) {
