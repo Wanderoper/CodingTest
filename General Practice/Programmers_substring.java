2cd00a92ee32c8e683feea391e6946f4a29dfa96
SAP 개발자 <quelqunadire@gmail.com>
2024-02-13 08:16:37 +0900

    (기초) 문자열 겹쳐쓰기
    주석추가

--- General Practice/Programmers_substring.java
+++ General Practice/Programmers_substring.java
@@ -1,7 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
+    	// 한번에 overwrite_string까지 붙여넣는다 
         String answer = my_string.substring(0,s) + overwrite_string;
         
+        // 만약 부족한 경우 치환이 끝난자리부터만 다시 붙인다 
         if(my_string.length() > answer.length()) {
             answer += my_string.substring(answer.length());
         }
