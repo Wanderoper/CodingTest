bf2aa2663dd74353e6a5960ac0d1910f0ae83c73
SAP 개발자 <quelqunadire@gmail.com>
2024-04-07 02:48:49 +0900

    Lv1. 12세 이하인 여자 환자 목록 출력하기

--- SQL/Programmers_select4.sql
+++ SQL/Programmers_select4.sql
@@ -1,3 +1,6 @@
+-- 카테고리 : SELECT
+-- Lv1. 12세 이하인 여자 환자 목록 출력하기
+
 -- ORACLE
 
 SELECT PT_NAME, PT_NO, GEND_CD, AGE, 
@@ -14,3 +17,20 @@ SELECT PT_NAME, PT_NO, GEND_CD, AGE,
 
 -- CASE 문 사용으로 null 처리
 -- 나이, 성별 조건 적용
+
+-- MYSQL
+
+SELECT PT_NAME,
+       PT_NO,
+       GEND_CD,
+       AGE,
+      (CASE
+        WHEN TLNO IS NULL THEN 'NONE'
+        ELSE TLNO
+       END) AS TLNO
+  FROM PATIENT
+ WHERE AGE <= 12 
+   AND GEND_CD = 'W'
+ ORDER BY
+       AGE DESC,
+       PT_NAME ASC;
